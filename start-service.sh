d467c11af0b240d9c95197a4821e898562b96f4d
aaron (kb) <aaron.sanders342@gmail.com>
2024-07-15 22:09:23 -0700

    debugging CodeDeploy

--- start-service.sh
+++ start-service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
+exec > /tmp/start-service.log 2>&1
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 npm run start
--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
+exec > /tmp/stop-service.log 2>&1
 source /home/ec2-user/.bash_profile
-[ -d "/home/ec2-user/app/release" ] && \
-cd /home/ec2-user/app/release && \
-npm stop
+if [ -d "/home/ec2-user/app/release" ]; then
+    cd /home/ec2-user/app/release && \
+    npm run stop
+fi
