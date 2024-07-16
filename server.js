20650ee2d946fd270a1abdc83c553c915c220ade
aaron (kb) <aaron.sanders342@gmail.com>
2024-07-15 22:32:21 -0700

    fix hostname log

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require('os');
 const http = require('http');
 
-const message = 'Hello World from ${hostname()}\n';
+const message = `Hello World from ${hostname()}\n`;
 const port = 8181;
 
 const server = http.createServer((req, res) => {
